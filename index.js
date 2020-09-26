1338d7fedac3e106373db16bfdc045acaffd6cbd
David Moll <david.leander.moll@gmail.com>
2020-09-27 01:28:37 +0200

    message-event now emits client

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on('message', async message => {
   if (!client.commands.has(command)) return;
 
   try {
-    client.commands.get(command).execute(message, args);
+    client.commands.get(command).execute(client, message, args);
   } catch (error) {
     console.error(error);
     message.reply('there was an error trying to execute that command!');
